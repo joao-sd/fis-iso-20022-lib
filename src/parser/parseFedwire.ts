c734114f395a13b335e80e8c458f23a5a3211070
Joao Paulo Furtado <joao.furtado@sada.com>
2024-07-30 10:36:48 -0700

    chore: Update file extensions to XML for Fedwire parser

--- src/parser/parseFedwire.ts
+++ src/parser/parseFedwire.ts
@@ -7,11 +7,11 @@ const filePaths = [
   // path.join(__dirname, "../data/PACS008_CustomerCreditTransfer.txt"),
   // path.join(__dirname, "../data/PACS008_USTreasuryTaxPayment.txt"),
 
-  path.join(DATA_PATH, 'PACS004_PaymentReturn.txt'),
-  path.join(DATA_PATH, 'PACS008_CustomerCreditTransfer.txt'),
-  path.join(DATA_PATH, 'PACS008_USTreasuryTaxPayment.txt'),
-  path.join(DATA_PATH, 'PACS009_BankCreditTransfer.txt'),
-  path.join(DATA_PATH, 'PACS009_CoverPayment.txt'),
+  path.join(DATA_PATH, 'PACS004_PaymentReturn.xml'),
+  path.join(DATA_PATH, 'PACS008_CustomerCreditTransfer.xml'),
+  path.join(DATA_PATH, 'PACS008_USTreasuryTaxPayment.xml'),
+  path.join(DATA_PATH, 'PACS009_BankCreditTransfer.xml'),
+  path.join(DATA_PATH, 'PACS009_CoverPayment.xml'),
 ];
 
 filePaths.forEach((filePath) => {
